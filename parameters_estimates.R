c18e2c6e96c6bdbf9c8c9b4b70c0b81f5b3a7b4e
Senerath de Silva <desilva.sen@gmail.com>
2018-09-04 22:54:13 +0800

    first commit

---
+++ parameters_estimates.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+library(dslabs)
+ds_theme_set()
+take_poll(25) 
+# dslabs equivalent to draw random sample of 25 beads from urn of blue and red beads
+# Computing estimate of std error
+X_bar <- 0.48
+se_hat <- sqrt(X_bar*(1 - X_bar)/25)
+se_hat # approx 0.100
+
+# Probability of being within 1% of p or population mean
+pnorm(0.01/se_hat) - pnorm(-0.01/se_hat) # 0.08 or 8%
+
+# Probability of 2 SEs from p or populaton mean
+pnorm(2) - pnorm(-2) # 95%
+
+## Monte Carlo Simulation for testing CLT on urn model
+# urn model - BUT cannot run due p unknown
+# B <- 10000 # beads in urn
+# N <- 1000 # sample size
+# X_bar <- replicate(B, {
+#   X <- sample(c(0,1), size = N, replace = TRUE, prob = c(1-p, p))
+#   mean(X)
+# })
+
+# testing for 1 example at p = 0.45
+# theory
+X_bar <- 0.45
+se_hat <- sqrt(X_bar*(1 - X_bar)/1000)
+se_hat # approx 0.0157 or 1.6%
+
+# monte carlo sim
+B <- 10000 # beads in urn
+N <- 1000 # sample size
+X_bar <- replicate(B, {
+  X <- sample(c(0,1), size = N, replace = TRUE, prob = c(1-0.45, 0.45))
+  mean(X)
+})
+sd(X_bar) # 0.0156 or 1.6%
+
+# plot histogram to check if distribution is normal
+hist(X_bar) # appears to be normal distribution
+
+# p <- 0.45
+# N <- 1000
+# X <- sample(c(0,1), size = N, replace = TRUE, prob = c(1-p,p))
+# X_bar <- mean(X)
+
+
+
+
