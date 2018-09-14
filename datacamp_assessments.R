e20f4e5817df128cb92283d40d092c6866be0f86
Senerath de Silva <desilva.sen@gmail.com>
2018-09-14 23:03:15 +0800

    added code

--- datacamp_assessments.R
+++ datacamp_assessments.R
@@ -66,3 +66,97 @@ x <- c(10, 20 ,30, 40, 50)
 sample(x, size = N, replace = TRUE)
 x
 
+### A6.1
+# Load the libraries and data
+library(dplyr)
+library(dslabs)
+data("polls_us_election_2016")
+
+# Create a table called `polls` that filters by  state, date, and reports the spread
+polls <- polls_us_election_2016 %>% 
+  filter(state != "U.S." & enddate >= "2016-10-31") %>% 
+  mutate(spread = rawpoll_clinton/100 - rawpoll_trump/100)
+
+# Create an object called `cis` that columns for the lower and upper confidence intervals.
+# Select the columns indicated in the instructions.
+# X_hat = estimate of proportion of Clinton supporters
+cis <- polls %>% 
+  mutate(X_hat = (spread + 1)/2, se = sqrt(X_hat*(1-X_hat)/samplesize), 
+         lower = spread - 2*qnorm(.975)*se, upper = spread + 2*qnorm(.975)*se) %>% 
+  select(state, startdate, enddate, pollster, grade, spread, lower, upper)
+head(cis)
+
+# Ex2
+# Add the actual results to the `cis` data set 
+add <- results_us_election_2016 %>% mutate(actual_spread = clinton/100 - trump/100) %>% 
+  select(state, actual_spread)
+cis <- cis %>% mutate(state = as.character(state)) %>% left_join(add, by = "state")
+head(cis)
+
+# Create an object called `p_hits` that summarizes the proportion of confidence intervals 
+# that contain the actual value. Print this object to the console.
+p_hits <- cis %>% mutate(hit = actual_spread >= lower & actual_spread <= upper) %>%
+  summarise(proportion_hits = mean(hit))
+p_hits
+
+
+# Ex3
+# The `cis` data have already been loaded for you
+add <- results_us_election_2016 %>% mutate(actual_spread = clinton/100 - trump/100) %>%
+  select(state, actual_spread)
+cis <- cis %>% mutate(state = as.character(state)) %>% left_join(add, by = "state")
+
+# Create an object called `p_hits` that summarizes the proportion of hits for each pollster 
+# that has more than 5 polls. (errors: want 5 or more; also wants column to be only 'n')
+p_hits <- cis %>%
+  mutate(hit = actual_spread >= lower & actual_spread <= upper) %>%
+  group_by(pollster) %>% 
+  filter(n() >= 5) %>%
+  summarise(proportion_hits = mean(hit), n = n(), grade = first(grade)) %>%
+  arrange(desc(proportion_hits))
+p_hits
+
+# Ex4
+# Create an object called `p_hits` that summarizes the proportion of hits for each state that has more than 5 polls.
+p_hits <- cis %>%
+  mutate(hit = actual_spread >= lower & actual_spread <= upper) %>%
+  group_by(state) %>%
+  filter(n() >= 5) %>%
+  summarise(proportion_hits = mean(hit), n = n()) %>%
+  arrange(desc(proportion_hits))
+p_hits
+
+# Ex6
+# The `cis` data have already been loaded. Examine it using the `head` function.
+head(cis)
+
+# Create an object called `errors` that calculates the difference between the predicted 
+# and actual spread and indicates if the correct winner was predicted
+errors <- cis %>% 
+  mutate(error = spread - actual_spread, hit = (spread > 0 & actual_spread > 0) | 
+           (spread < 0 & actual_spread < 0) )
+
+# Examine the last 6 rows of `errors`
+tail(errors)
+
+# Ex7
+# Create an object called `errors` that calculates the difference between the predicted and actual spread and indicates if the correct winner was predicted
+errors <- cis %>% mutate(error = spread - actual_spread, hit = sign(spread) == sign(actual_spread))
+
+# Create an object called `p_hits` that summarizes the proportion of hits for each state that has more than 5 polls
+p_hits <- errors %>% group_by(state) %>% filter(n() >= 5) %>%
+  summarise(proportion_hits = mean(hit), n = n()) 
+
+
+# Make a barplot of the proportion of hits for each state
+library(ggplot2)
+ggplot(p_hits, aes(x = state, y = proportion_hits)) +
+  geom_bar(stat = "identity") +
+  coord_flip()
+
+# Ex9
+# Create a boxplot showing the errors by state for polls with grades B+ or higher
+errors %>% filter(grade %in% c("B+", "A-", "A", "A+")) %>%
+  mutate(state = reorder(state, error)) %>% ggplot(aes(x=state, y=error)) +
+  geom_boxplot() +
+  geom_point()
